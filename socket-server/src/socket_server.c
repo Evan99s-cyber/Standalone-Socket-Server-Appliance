d6d5220cf7c3e7ddd23bd928ab7dbbe184d5eb88
evsp9901 <evsp9901@Ubuntu.myguest.virtualbox.org>
2026-04-28 04:37:12 -0400

    Script updates

--- socket-server/src/socket_server.c
+++ socket-server/src/socket_server.c
@@ -1,35 +1,22 @@
 #include <stdio.h>
-
 #include <stdlib.h>
-
 #include <signal.h>
-
 #include <unistd.h>
-
 #include <string.h>
-
 #include <sys/socket.h>
-
 #include <netinet/in.h>
-
  
 
 #define PORT 9000
 
- 
-
 static int server_fd = -1;
-
- 
 
 static void handle_signal(int sig)
 
 {
 
     (void)sig;
-
     if (server_fd >= 0) {
-
         close(server_fd);
 
     }
@@ -38,21 +25,13 @@ static void handle_signal(int sig)
 
 }
 
- 
-
 int main(void)
 
 {
-
-    struct sockaddr_in addr;
-
- 
+   struct sockaddr_in addr;
 
     signal(SIGINT, handle_signal);
-
     signal(SIGTERM, handle_signal);
-
- 
 
     server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
@@ -67,45 +46,51 @@ int main(void)
  
 
     memset(&addr, 0, sizeof(addr));
-
     addr.sin_family = AF_INET;
-
     addr.sin_addr.s_addr = INADDR_ANY;
-
     addr.sin_port = htons(PORT);
 
  
 
     if (bind(server_fd, (struct sockaddr *)&addr, sizeof(addr)) < 0) {
-
         perror("bind");
-
         close(server_fd);
-
         return 1;
 
     }
-
- 
 
     if (listen(server_fd, 5) < 0) {
-
         perror("listen");
-
         close(server_fd);
-
         return 1;
 
     }
 
- 
+    int client_fd;
+    struct sockaddr_in client_addr;
+    socklen_t client_len = sizeof(client_addr);
+    char buffer[1024];
+
+    printf("Server listening on port %d...\n", PORT);
 
     while (1) {
+        // This blocks until a client actually connects
+        client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &client_len);
+        if (client_fd < 0) {
+            perror("accept failed");
+            continue;
+        }
 
-        pause();
+        // Optional: Read incoming data to prove it works
+        memset(buffer, 0, sizeof(buffer));
+        int bytes_read = read(client_fd, buffer, sizeof(buffer) - 1);
+        if (bytes_read > 0) {
+            printf("Received: %s\n", buffer);
+        }
 
+        // Close the client connection and loop back to wait for the next one
+        close(client_fd);
     }
-
  
 
     return 0;
