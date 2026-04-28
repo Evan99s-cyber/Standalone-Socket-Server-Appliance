2825194ce4b2dd1cf92ec60270c97da484e159dd
evsp9901 <evsp9901@Ubuntu.myguest.virtualbox.org>
2026-04-28 05:10:04 -0400

    Make fixes

--- package/socketserver/socketserver.mk
+++ package/socketserver/socketserver.mk
@@ -1,27 +1,15 @@
 ##############################################################################
-
 #
-
 # socketserver
-
 #
-
 ################################################################################
 
- 
-
 SOCKETSERVER_VERSION = 1.0
-
 SOCKETSERVER_SITE = $(BR2_EXTERNAL_SOCKETSERVER_PATH)/socket-server
-
 SOCKETSERVER_SITE_METHOD = local
 
- 
-
 SOCKETSERVER_LICENSE = MIT
-
 SOCKETSERVER_LICENSE_FILES = LICENSE
-
  
 
 define SOCKETSERVER_BUILD_CMDS
@@ -30,14 +18,10 @@ define SOCKETSERVER_BUILD_CMDS
 
 endef
 
- 
-
 define SOCKETSERVER_INSTALL_TARGET_CMDS
 
     $(INSTALL) -D -m 0755 </span>
-
         $(@D)/socket_server </span>
-
         $(TARGET_DIR)/usr/bin/socket_server
 
 endef
